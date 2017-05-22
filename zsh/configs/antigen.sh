67798144857590be87cb30bb90ed663f31a0cc45
John Eckhart <jweckhart@gmail.com>
2017-05-22 01:38:50 -0400

    Add some zsh customizations

---
+++ zsh/configs/antigen.sh
@@ -0,0 +1,25 @@
+
+source /usr/local/share/antigen/antigen.zsh
+antigen use oh-my-zsh
+
+if [ "$OSTYPE"="darwin15.0" ]; then
+  antigen bundle osx
+  antigen bundle brew
+fi
+
+antigen bundles <<EOBUNDLES
+  git
+  git-extras
+  pip
+  colored-man-pages
+  gnu-utils
+  history
+  zsh-users/zsh-syntax-highlighting
+  zsh-users/zsh-completions
+  nojhan/liquidprompt
+EOBUNDLES
+
+antigen theme gentoo
+
+antigen apply
+
