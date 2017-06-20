ecafd1a9d22f4b45506db1968b95a7f66b3f9aab
John Eckhart <jweckhart@gmail.com>
2017-06-20 14:59:49 -0400

    Add home and end keybindings

--- zsh/configs/zz_keybindings.local.zsh
+++ zsh/configs/zz_keybindings.local.zsh
@@ -1,4 +1,6 @@
 [[ "${terminfo[kpp]}" != "" ]] && bindkey "${terminfo[kpp]}" up-line-or-search   # [PageUp] - search forward
 [[ "${terminfo[knp]}" != "" ]] && bindkey "${terminfo[knp]}" down-line-or-search # [PageDown] - search backward
+[[ "${terminfo[khome]}" != "" ]] && bindkey "${terminfo[khome]}" beginning-of-line # [Home] - search backward
+[[ "${terminfo[kend]}" != "" ]] && bindkey "${terminfo[kend]}" end-of-line       # [End] - search backward
 bindkey '^[[1;6C' forward-word                                                   # [Ctrl-Shift-RightArrow] - move forward one word
 bindkey '^[[1;6D' backward-word                                                  # [Ctrl-Shift-LeftArrow] - move backward one word
