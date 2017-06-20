618c75dbf4b2e077bf344463807de5e31567ccee
John Eckhart <jweckhart@gmail.com>
2017-06-20 14:56:37 -0400

    Add binding to jump fwd/back a word

--- zsh/configs/zz_keybindings.local.zsh
+++ zsh/configs/zz_keybindings.local.zsh
@@ -1,6 +1,4 @@
-if [[ "${terminfo[kpp]}" != "" ]]; then
-  bindkey "${terminfo[kpp]}" up-line-or-search       # [PageUp] - search forward
-fi
-if [[ "${terminfo[knp]}" != "" ]]; then
-  bindkey "${terminfo[knp]}" down-line-or-search     # [PageDown] - search backward
-fi
+[[ "${terminfo[kpp]}" != "" ]] && bindkey "${terminfo[kpp]}" up-line-or-search   # [PageUp] - search forward
+[[ "${terminfo[knp]}" != "" ]] && bindkey "${terminfo[knp]}" down-line-or-search # [PageDown] - search backward
+bindkey '^[[1;6C' forward-word                                                   # [Ctrl-Shift-RightArrow] - move forward one word
+bindkey '^[[1;6D' backward-word                                                  # [Ctrl-Shift-LeftArrow] - move backward one word
