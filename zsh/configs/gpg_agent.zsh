670a2aaa61c6698e068696095d6f77c601e6fa5b
John Eckhart <johne@nearpod.com>
2019-10-19 16:30:02 -0400

    A few minor gpg settings to make the ssh-agent work systemwide

--- zsh/configs/gpg_agent.zsh
+++ zsh/configs/gpg_agent.zsh
@@ -1,3 +1,3 @@
 export GPG_TTY=$(tty)
 gpg-connect-agent /bye
-export SSH_AUTH_SOCK=$(/usr/local/MacGPG2/bin/gpgconf --list-dirs agent-ssh-socket)
+# export SSH_AUTH_SOCK=$(/usr/local/MacGPG2/bin/gpgconf --list-dirs agent-ssh-socket)
