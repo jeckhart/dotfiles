4d50dbd293bc1a998c1d2b9e6503f1827bf24111
John Eckhart <jweckhart@gmail.com>
2020-08-22 15:28:59 -0400

    Move antigen config to antigenrc which is a little faster

--- zsh/configs/antigen.zsh
+++ zsh/configs/antigen.zsh
@@ -1,28 +1,5 @@
 
 source /usr/local/share/antigen/antigen.zsh
-antigen use oh-my-zsh
 
-if [ "$OSTYPE"="darwin15.0" ]; then
-  antigen bundle osx
-  antigen bundle brew
-fi
-
-antigen bundles <<EOBUNDLES
-  fzf
-  git
-  git-extras
-  pip
-  colored-man-pages
-  gnu-utils
-  history
-  zsh-users/zsh-syntax-highlighting
-  zsh-users/zsh-completions
-  nojhan/liquidprompt
-  kubectl
-  matthieusb/zsh-sdkman
-EOBUNDLES
-
-antigen theme gentoo
-
-antigen apply
+antigen init ~/.antigenrc
 
