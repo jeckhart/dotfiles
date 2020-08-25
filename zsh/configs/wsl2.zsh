6dc20c5426605e049ed5ae2bc7c31455ac075785
John Eckhart <jweckhart@gmail.com>
2020-08-25 13:31:40 -0400

    Add keybase support to WSL2

--- zsh/configs/wsl2.zsh
+++ zsh/configs/wsl2.zsh
@@ -16,5 +16,18 @@ if [ -e /proc ] && [ $(grep -oE 'gcc version ([0-9]+)' /proc/version | awk '{pri
     setsid nohup socat UNIX-LISTEN:$GPG_AGENT_SOCK,fork EXEC:"$HOME/.ssh/wsl2-ssh-pageant.exe --gpg S.gpg-agent" >/dev/null 2>&1 &
   fi
 
+  ps ax | grep -q /usr/bin/kbfsfuse
+  if [ $? -ne 0 ]; then
+    setsid nohup /usr/bin/kbfsfuse -debug -log-to-file >/dev/null 2>&1 &
+  fi
+  ps ax | grep -q /usr/bin/keybase
+  if [ $? -ne 0 ]; then
+    setsid nohup /usr/bin/keybase --use-default-log-file --debug service >/dev/null 2>&1 &
+  fi
+
+#  setsid nohup /usr/bin/kbfsfuse -debug -log-to-file >/dev/null 2>&1 &
+#  setsid nohup /usr/bin/keybase --use-default-log-file --debug service >/dev/null 2>&1 &
+  # /usr/bin/keybase --debug --log-file /home/jeckhart/.cache/keybase/keybase.service.log service --chdir /home/jeckhart/.config/keybase --auto-forked
+  # /mnt/wsl/docker-desktop/docker-desktop-proxy --distro-name Ubuntu --docker-desktop-root /mnt/wsl/docker-desktop
   wslfetch
 fi
