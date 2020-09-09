f810eeca7bb16382711dbdfd6e0456177a4e8c04
John Eckhart <jweckhart@gmail.com>
2020-09-09 11:19:29 -0400

    Set home paths for Java, Graal, Maven, and Gradle via sdkman

---
+++ zsh/configs/sdkman.zsh
@@ -0,0 +1,7 @@
+
+export GRAALVM_HOME=~/.sdkman/candidates/java/20.0.0.r11-grl
+export MAVEN_HOME=~/.sdkman/candidates/maven/current
+export GRADLE_HOME=~/.sdkman/candidates/gradle/current
+export JAVA_HOME=~/.sdkman/candidates/java/current
+#export PATH="$PATH":$GRAALVM_HOME/bin
+
