810b19290222cf190e629e9a6ca3b5ab91fb8ff5
John Eckhart <jweckhart@gmail.com>
2020-10-27 15:04:48 -0400

    Lets put our ssh and gpg sockets there so they work across distros
    WSL2 mounts a directory at /mnt/wsl that is shared across all running distros.
    
    If we put our ssh and gpg agent sockets there, we can reuse them across multiple running distros

--- zsh/configs/wsl2.zsh
+++ zsh/configs/wsl2.zsh
@@ -3,17 +3,24 @@
 if [ -e /proc ] && [ $(grep -oE 'gcc version ([0-9]+)' /proc/version | awk '{print $3}') -gt 5 ] ; then
   export LC_ALL=en_US.UTF-8
 
-  export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-  ss -a | grep -q $SSH_AUTH_SOCK
+  SHARED_CONFIG=/mnt/wsl/shared
+  mkdir -p "$SHARED_CONFIG"
+
+  export SSH_AUTH_SOCK="$SHARED_CONFIG/S.ssh-agent.sock"
+  ss -a | grep -q "$SSH_AUTH_SOCK"
   if [ $? -ne 0 ]; then
-    rm -f $SSH_AUTH_SOCK
-    setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:$HOME/.ssh/wsl2-ssh-pageant.exe >/dev/null 2>&1 &
+    rm -f "$SSH_AUTH_SOCK"
+    setsid nohup socat UNIX-LISTEN:"$SSH_AUTH_SOCK",fork EXEC:$HOME/.ssh/wsl2-ssh-pageant.exe >/dev/null 2>&1 &
   fi
-  export GPG_AGENT_SOCK=$HOME/.gnupg/S.gpg-agent
-  ss -a | grep -q $GPG_AGENT_SOCK
+  export GPG_AGENT_SOCK="$SHARED_CONFIG/S.gpg-agent.sock"
+  ss -a | grep -q "$GPG_AGENT_SOCK"
   if [ $? -ne 0 ]; then
-    rm -rf $GPG_AGENT_SOCK
-    setsid nohup socat UNIX-LISTEN:$GPG_AGENT_SOCK,fork EXEC:"$HOME/.ssh/wsl2-ssh-pageant.exe --gpg S.gpg-agent" >/dev/null 2>&1 &
+    rm -rf "$GPG_AGENT_SOCK"
+    setsid nohup socat UNIX-LISTEN:"$GPG_AGENT_SOCK",fork EXEC:"$HOME/.ssh/wsl2-ssh-pageant.exe --gpg S.gpg-agent" >/dev/null 2>&1 &
+  fi
+  if [ ! -L "$HOME/.gnupg/S.gpg-agent" ]; then
+    rm -f "$HOME/.gnupg/S.gpg-agent"
+    ln -nsf "$GPG_AGENT_SOCK" "$HOME/.gnupg/S.gpg-agent"
   fi
 
   ps ax | grep -q /usr/bin/kbfsfuse
