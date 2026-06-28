c0fee860519ad8be95d22a45c8b905d7118d0134
John Eckhart <jeckhart@hey.com>
2026-06-28 02:52:31 -0400

    chore(zsh): drop unused keybase/kbfsfuse from WSL2 config + Brewfile
    Keybase is no longer used. Remove the kbfsfuse + keybase service startup blocks
    (and dead commented scratch lines) from wsl2.zsh, leaving only locale, X11
    DISPLAY, SSH agent forwarding, and wslfetch. Also drop the stale commented-out
    keybase cask from the Brewfile.

--- dot_config/zsh/configs/wsl2.zsh
+++ dot_config/zsh/configs/wsl2.zsh
@@ -16,18 +16,5 @@ if [ -e /proc ] && $(grep -oE 'WSL2' /proc/version >/dev/null 2>&1 ) ; then
     setsid nohup socat UNIX-LISTEN:"$SSH_AUTH_SOCK",fork EXEC:$HOME/.ssh/wsl2-ssh-pageant.exe >/dev/null 2>&1 &
   fi
 
-  ps ax | grep -q /usr/bin/kbfsfuse
-  if [ $? -ne 0 ]; then
-    setsid nohup /usr/bin/kbfsfuse -debug -log-to-file >/dev/null 2>&1 &
-  fi
-  ps ax | grep -q /usr/bin/keybase
-  if [ $? -ne 0 ]; then
-    setsid nohup /usr/bin/keybase --use-default-log-file --debug service >/dev/null 2>&1 &
-  fi
-
-#  setsid nohup /usr/bin/kbfsfuse -debug -log-to-file >/dev/null 2>&1 &
-#  setsid nohup /usr/bin/keybase --use-default-log-file --debug service >/dev/null 2>&1 &
-  # /usr/bin/keybase --debug --log-file /home/jeckhart/.cache/keybase/keybase.service.log service --chdir /home/jeckhart/.config/keybase --auto-forked
-  # /mnt/wsl/docker-desktop/docker-desktop-proxy --distro-name Ubuntu --docker-desktop-root /mnt/wsl/docker-desktop
   wslfetch
 fi
